92cad31036cdb81407e7286c4273e72ed0448662
Ada Keita <mathildebrakstad@gmail.com>
2023-05-01 02:51:27 +0200

    domain

--- soilmates/scripts/blogposts.js
+++ soilmates/scripts/blogposts.js
@@ -1,58 +1,57 @@
 function displayBlogPosts(posts) {
-    const blogPostsContainer = document.querySelector(".blog-list-container");
-  
-    posts.forEach((post) => {
-      const postTitle = post.acf["post-title"];
-      const postExcerpt = post.acf["blog-text"].substring(0, 100) + "...";
-      const postId = post.id;
-      const postImage = post.acf["post-image"];
-  
-      const postHTML = `
-        <a href="blogpost.html?id=${postId}">
+	const blogPostsContainer = document.querySelector(".blog-list-container");
+
+	posts.forEach((post) => {
+		const postTitle = post.acf["post-title"];
+		const postExcerpt = post.acf["blog-text"].substring(0, 100) + "...";
+		const postId = post.id;
+		const postImage = post.acf["post-image"];
+
+		const postHTML = `
+        <a class="post-redirect" href="blogpost.html?id=${postId}">
           <div class="blog-post" style="background-image: url('${postImage}');">
-            <div class="blog-post-content content-font">
-              <h2 class="blogtitle-list">${postTitle}</h2>
-            </div>
+              <div class="blogpost-stylingwrapper">
+                <div class="blog-post-content content-font">
+                  <h2 class="blogtitle-list">${postTitle}</h2>
+                </div>
+              <div>
           </div>
         </a>
       `;
-  
-      blogPostsContainer.insertAdjacentHTML("beforeend", postHTML);
-    });
-  }
-  
-  
+
+		blogPostsContainer.insertAdjacentHTML("beforeend", postHTML);
+	});
+}
 
 let startIndex = 0;
 const postsPerPage = 10;
 
 async function loadBlogPosts() {
-    showLoadingIndicator();
-    console.log("Loading blog posts");
-    const posts = getBlogPosts(startIndex, postsPerPage);
-    console.log("Blog posts to display:", posts);
-    displayBlogPosts(posts);
-    startIndex += postsPerPage;
-    hideLoadingIndicator();
-  }
+	showLoadingIndicator();
+	console.log("Loading blog posts");
+	const posts = getBlogPosts(startIndex, postsPerPage);
+	console.log("Blog posts to display:", posts);
+	displayBlogPosts(posts);
+	startIndex += postsPerPage;
+	hideLoadingIndicator();
+}
 
+async function displayBlogPost() {
+	showLoadingIndicator();
 
-  async function displayBlogPost() {
-    showLoadingIndicator();
-  
-    const titleElement = document.querySelector(".page-header");
-    const contentElement = document.querySelector(".page-text-container");
-    const blogPostWrapper = document.querySelector(".blogpost-wrapper");
-  
-    const postId = parseInt(new URLSearchParams(window.location.search).get("id"));
-    const post = await getBlogPostById(postId);
-  
-    if (post) {
-      const postData = post.acf;
-      const postImage = postData["post-image"];
-  
-      titleElement.innerHTML = postData["post-title"];
-      contentElement.innerHTML = `
+	const titleElement = document.querySelector(".page-header");
+	const contentElement = document.querySelector(".page-text-container");
+	const blogPostWrapper = document.querySelector(".blogpost-wrapper");
+
+	const postId = parseInt(new URLSearchParams(window.location.search).get("id"));
+	const post = await getBlogPostById(postId);
+
+	if (post) {
+		const postData = post.acf;
+		const postImage = postData["post-image"];
+
+		titleElement.innerHTML = postData["post-title"];
+		contentElement.innerHTML = `
         <div class="blogpost-img-wrapper">
           <img src="${postImage}" alt="Blog post image">
         </div>
@@ -60,29 +59,24 @@ async function loadBlogPosts() {
           <p>${postData["blog-text"].replace(/\r\n/g, "</p><p>")}</p>
         </div>
       `;
-    } else {
-      titleElement.innerHTML = "Post not found";
-      contentElement.innerHTML = "<p>We couldn't find the post you're looking for. Please go back and try again.</p>";
-    }
-  
-    hideLoadingIndicator();
-  }
-  
-  
-  
+	} else {
+		titleElement.innerHTML = "Post not found";
+		contentElement.innerHTML =
+			"<p>We couldn't find the post you're looking for. Please go back and try again.</p>";
+	}
+
+	hideLoadingIndicator();
+}
 
 // Add this init function instead
 document.addEventListener("DOMContentLoaded", async () => {
-    await fetchAllPosts();
-  
-    if (window.location.pathname.includes("posts.html")) {
-        const viewMoreButton = document.getElementById("view-more-button");
-        viewMoreButton.addEventListener("click", loadBlogPosts);
-        loadBlogPosts();
-      } else if (window.location.pathname.includes("blogpost.html")) {
-        displayBlogPost();
-      }
-      
-  });
-  
-  
+	await fetchAllPosts();
+
+	if (window.location.pathname.includes("posts.html")) {
+		const viewMoreButton = document.getElementById("view-more-button");
+		viewMoreButton.addEventListener("click", loadBlogPosts);
+		loadBlogPosts();
+	} else if (window.location.pathname.includes("blogpost.html")) {
+		displayBlogPost();
+	}
+});
--- soilmates/scripts/fetchData.js
+++ soilmates/scripts/fetchData.js
@@ -2,59 +2,55 @@
 let allPosts = [];
 
 async function fetchAllPosts() {
-    showLoadingIndicator();
-    try {
-      const allFetchedPosts = [];
-      const perPage = 100;
-      let currentPage = 1;
-      let reachedEnd = false;
-  
-      while (!reachedEnd) {
-        const response = await fetch(
-          `http://localhost/soilmates-api/wp-json/acf/v3/blogpost?per_page=${perPage}&page=${currentPage}`
-        );
-        
-        if (!response.ok) {
-          reachedEnd = true;
-          continue;
-        }
-  
-        const data = await response.json();
-        allFetchedPosts.push(...data);
-  
-        if (data.length < perPage) {
-          reachedEnd = true;
-        } else {
-          currentPage++;
-        }
-      }
-  
-      allPosts = allFetchedPosts;
-    } catch (error) {
-      console.error("Error fetching all posts:", error);
-    }
-    console.log("Fetched all posts:", allPosts);
-    hideLoadingIndicator();
-  }
-  
+	showLoadingIndicator();
+	try {
+		const allFetchedPosts = [];
+		const perPage = 100;
+		let currentPage = 1;
+		let reachedEnd = false;
+
+		while (!reachedEnd) {
+			const response = await fetch(
+				`https://api.adakeita.dev/wp-json/acf/v3/blogpost?per_page=${perPage}&page=${currentPage}`
+			);
+
+			if (!response.ok) {
+				reachedEnd = true;
+				continue;
+			}
+
+			const data = await response.json();
+			allFetchedPosts.push(...data);
+
+			if (data.length < perPage) {
+				reachedEnd = true;
+			} else {
+				currentPage++;
+			}
+		}
+
+		allPosts = allFetchedPosts;
+	} catch (error) {
+		console.error("Error fetching all posts:", error);
+	}
+	console.log("Fetched all posts:", allPosts);
+	hideLoadingIndicator();
+}
 
 function getLatestPosts(startIndex = 0, perPage = 4) {
-    return allPosts.slice(startIndex, startIndex + perPage).reverse();
-  }  
+	return allPosts.slice(startIndex, startIndex + perPage).reverse();
+}
 
-  function getBlogPosts(startIndex = 0, perPage = 10) {
-    console.log("Fetched all posts:", allPosts);
-    return allPosts.slice(startIndex, startIndex + perPage);
-  }
-  
+function getBlogPosts(startIndex = 0, perPage = 10) {
+	console.log("Fetched all posts:", allPosts);
+	return allPosts.slice(startIndex, startIndex + perPage);
+}
 
-  function getBlogPostById(id) {
-    console.log("Searching for post with ID:", id);
-    return allPosts.find((post) => parseInt(post.id, 10) == id);
-  }
-  
-  
+function getBlogPostById(id) {
+	console.log("Searching for post with ID:", id);
+	return allPosts.find((post) => parseInt(post.id, 10) == id);
+}
 
 document.addEventListener("DOMContentLoaded", async () => {
-  await fetchAllPosts();
+	await fetchAllPosts();
 });
