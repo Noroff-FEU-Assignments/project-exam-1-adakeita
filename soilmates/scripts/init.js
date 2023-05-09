ac7251d48f3d21374a2666467157034f0e683e1d
Ada Keita <mathildebrakstad@gmail.com>
2023-05-09 18:52:45 +0200

    authv2

--- soilmates/scripts/init.js
+++ soilmates/scripts/init.js
@@ -1,55 +1,56 @@
 import {
-    displayComments,
-    fetchAllPosts,
-    getBlogPosts,
-    fetchCommentsForPost,
-    getPostById,
+	displayComments,
+	fetchAllPosts,
+	getBlogPosts,
+	fetchCommentsForPost,
+	getPostById,
 } from "./utils.js";
 import { setupCarousel } from "./carousel.js";
 import {
-    displayBlogPost,
-    displayBlogList,
-    loadBlogPosts,
-    moveImageContainer,
+	displayBlogPost,
+	displayBlogList,
+	loadBlogPosts,
+	moveImageContainer,
 } from "./blogposts.js";
 import {
-    setupLoginButton,
-    setupLogoutButton,
-    setupSubmitCommentButton,
+	setupLoginButton,
+	setupLogoutButton,
+	setupSubmitCommentButton,
 } from "./buttonhandlers.js";
+import { initAuth0 } from "./auth.js";
 
 window.addEventListener("resize", moveImageContainer);
 
 document.addEventListener("DOMContentLoaded", async () => {
-    await fetchAllPosts();
+	await initAuth0();
+	await fetchAllPosts();
 
-    if (
-        window.location.pathname.includes("index.html") ||
-        window.location.pathname === "/"
-    ) {
-        setupCarousel();
-        setupLoginButton();
-        setupLogoutButton();
-    } else if (window.location.pathname.includes("posts.html")) {
-        const posts = getBlogPosts();
-        displayBlogList(posts);
-        const viewMoreButton = document.getElementById("view-more-button");
-        let startIndex = 0;
-        const postsPerPage = 10;
+	if (
+		window.location.pathname.includes("index.html") ||
+		window.location.pathname === "/"
+	) {
+		setupCarousel();
+		setupLoginButton();
+		setupLogoutButton();
+	} else if (window.location.pathname.includes("posts.html")) {
+		const posts = getBlogPosts();
+		displayBlogList(posts);
+		const viewMoreButton = document.getElementById("view-more-button");
+		let startIndex = 0;
+		const postsPerPage = 10;
 
-        viewMoreButton.addEventListener("click", async () => {
-            startIndex += postsPerPage;
-            await loadBlogPosts(startIndex, postsPerPage);
-        });
-    } else if (window.location.pathname.includes("blogpost.html")) {
-        const postId = window.location.search.split("=")[1]; // Get the post ID from the URL
-        const post = getPostById(postId); // Get the blog post using the ID
-        displayBlogPost(post); // Display the blog post
+		viewMoreButton.addEventListener("click", async () => {
+			startIndex += postsPerPage;
+			await loadBlogPosts(startIndex, postsPerPage);
+		});
+	} else if (window.location.pathname.includes("blogpost.html")) {
+		const postId = window.location.search.split("=")[1]; // Get the post ID from the URL
+		const post = getPostById(postId); // Get the blog post using the ID
+		displayBlogPost(post); // Display the blog post
 
-        const comments = await fetchCommentsForPost(postId); // Fetch comments for the blog post
-        displayComments(comments);
+		const comments = await fetchCommentsForPost(postId); // Fetch comments for the blog post
+		displayComments(comments);
 
-        setupSubmitCommentButton(postId);
-    }
+		setupSubmitCommentButton(postId);
+	}
 });
-
